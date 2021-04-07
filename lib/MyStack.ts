6f3ea0f9c933851b68b881b19a5ea40709ad61bc
mlg_Vu.Van.Thanh.Luan <luan.vu@monstar-lab.com>
2021-04-07 11:28:44 +0700

    Add sst

---
+++ lib/MyStack.ts
@@ -0,0 +1,19 @@
+import * as sst from "@serverless-stack/resources";
+
+export default class MyStack extends sst.Stack {
+  constructor(scope: sst.App, id: string, props?: sst.StackProps) {
+    super(scope, id, props);
+
+    // Create the HTTP API
+    const api = new sst.Api(this, "Api", {
+      routes: {
+        "GET /": "src/lambda.handler",
+      },
+    });
+
+    // Show API endpoint in output
+    this.addOutputs({
+      "ApiEndpoint": api.httpApi.apiEndpoint,
+    });
+  }
+}
